de793ee73d3b1940dedfe3ba0838dad628ab2c36
Bryanlover1 <165657669+Bryanlover1@users.noreply.github.com>
2025-05-17 07:16:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -59,18 +59,18 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +916909137213")))
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +23353×××××")))
             process.exit(0)
          }
       } else {
-         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +916909137213 : `)))
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +23353××××× : `)))
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          // Ask again when entering the wrong number
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +254710xxxxx")))
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +23353873xxxxx")))
 
-            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +25471077xxx : `)))
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +233538733xxx : `)))
             phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
             rl.close()
          }
@@ -87,7 +87,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
         const { connection, lastDisconnect } = s
         if (connection == "open") {
             await delay(1000 * 10)
-            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `🪀Support/Contact Developer\n\n\n⎆Donate: https://telegra.ph/file/0c225f7da5616cdcbec80.jpg\n\n⎆YouTube: https://youtube.com/ibrahimaitech\n\n⎆Telegram Channel: https://t.me/ibrahimtechai\n\n⎆WhatsApp Channel: https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y\n\n⎆WhatsApp Pm: Wa.me/25471077266\n\n⎆Instagram: https://instagram.com/ibrahim_adams_official\n\n⎆GitHub: https://github.com/ibrahimaitech/\n\n⎆Blog: https://dreamguyxeonfiles.blogspot.com/2022/05/bots%20whatsapp%20mods.html?m=1\n\n\n` });
+            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `🪀Support/Contact Developer\n\n\n⎆Donate: https://files.catbox.moe/m1r863.jpg\n\n⎆YouTube: https://youtube.com/ibrahimaitech\n\n⎆Telegram Channel: https://t.me/ibrahimtechai\n\n⎆WhatsApp Channel: https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y\n\n⎆WhatsApp Pm: Wa.me/25471077266\n\n⎆Instagram: https://instagram.com/ibrahim_adams_official\n\n⎆GitHub: https://github.com/ibrahimaitech/\n\n⎆Blog: https://dreamguyxeonfiles.blogspot.com/2022/05/bots%20whatsapp%20mods.html?m=1\n\n\n` });
             let sessionXeon = fs.readFileSync('./sessions/creds.json');
             await delay(1000 * 2) 
              const xeonses = await  XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` })
@@ -95,9 +95,9 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
 ┌─❖
 │ Hello
 └┬❖  
-┌┤✑  Thanks for using ʙᴍᴡ ᴍᴅ
+┌┤✑  Thanks for using 𝖐𝖍𝖆𝖔𝖘 ᴍᴅ
 │└────────────┈ ⳹        
-│ > © sɪʀ ɪʙʀᴀʜɪᴍ
+│ > © 𝖎 𝖆𝖒 𝖐𝖍𝖆𝖔𝖘
 └─────────────────┈ ⳹\n\n ` }, {quoted: xeonses});
               await delay(1000 * 2) 
               process.exit(0)
